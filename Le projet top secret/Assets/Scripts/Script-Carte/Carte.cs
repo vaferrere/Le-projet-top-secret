938c01f6a15878e43dc7e3fffabe5005271c0c77
vaferrere <gadoy512@gmail.com>
2016-03-30 18:59:43 +0200

    Le commit des familles

---
+++ Le projet top secret/Assets/Scripts/Script-Carte/Carte.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Carte : MonoBehaviour {
+
+    public string nom;
+    public string description;
+    public string image;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Le projet top secret/Assets/Scripts/Script-Carte/CarteSoldat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Classe { Guerrier,Archer,Catapulte};
+public enum Capacite { Rien , Entraide , Medecin , Ralliement , Espion , Charge , Moral  };
+public enum Faction { Neutre , RoyaumeDuNord , Monstre , Nilfgaard , Scoiateal };
+public class CarteSoldat : Carte {
+
+    public int valeur;
+
+    public Classe classe;
+
+    public Capacite capacite;
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
